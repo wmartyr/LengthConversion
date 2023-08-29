c8645a1ee992cd768dae553c3a7736c534af2a5c
Woodrow Martyr <woodrow.martyr@gmail.com>
2023-08-29 21:39:22 +0930

    Initial Commit

---
+++ LengthConversion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LengthConversion
+//
+//  Created by Woodrow Martyr on 29/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Woodrow Martyr on 29/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
